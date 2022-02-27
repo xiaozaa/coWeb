f4ea9367c7b308a9389a37a11adb64fd3d7d1b83
anzhang <zhangan@anzhangs-MacBook-Pro.local>
2022-02-27 15:33:41 -0800

    fix type

--- catchonlab/src/footer/index.js
+++ catchonlab/src/footer/index.js
@@ -5,7 +5,7 @@ import styles from "./index.module.css";
 export const Footer = () => {
   return (
     <div className={styles.footerWrapper}>
-      <p>Copyright catchon labs 2020</p>
+      <p>Copyright catchon labs 2022</p>
     </div>
   );
 };
